06fad933a4d678ebe8b654910acb84522c42d8fd
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2024-08-28 10:44:42 +0200

    update Result.js

--- assets/js/Result.js
+++ assets/js/Result.js
@@ -1,30 +1,7 @@
-const Result = {
-  props: ["form"],
-  methods: {
-    async copyToClipboard() {
-      const resultText = document.getElementById("result-code").innerText;
-      const button = document.getElementById("copy-button");
-
-      try {
-        await navigator.clipboard.writeText(resultText);
-        console.log("Copied to clipboard!");
-
-        button.innerText = "Copied!";
-
-        setTimeout(() => {
-          button.innerText = "Copy";
-        }, 2000);
-      } catch (err) {
-        console.error("Failed to copy: ", err);
-      }
-    },
-  },
-  template: `
-    <div>
-      <h2>Result</h2>
-      <p>Paste this prompt into your AI assistant:</p>
-      <button id="copy-button" class="button" role="button" @click="copyToClipboard">Copy</button>
-      <pre><code id="result-code">## Request
+// Note that inside a pre tag all the whitespace is rendered, which is not good
+// for this purpose. Since each template on a newline adds whitespace, that is
+// better stripped out after. And indenting is also better stripped out.
+const outputCode = `## Request
 Write an answer using the following request and provide the answer only, without any preamble.
 
 {{ form.topic }}
@@ -42,17 +19,17 @@ Ensure that your answer follows what is outlined here.
 The audience is {{ form.audience }}.
 </template>
 <template v-if="form.asCodeblock || form.format || form.outputLength">
-### Output format
-<template v-if="form.asCodeblock">
-Output as a codeblock with code written in {{ form.asCodeblock }}.
-</template>
-<template v-if="form.format">
-Write the content as {{ form.format }}.
-</template>
-<template v-if="form.outputLength">
-### Length
-The content length should be {{ form.outputLength }}.
-</template>
+  ### Output format
+  <template v-if="form.asCodeblock">
+  Output as a codeblock with code written in {{ form.asCodeblock }}.
+  </template>
+  <template v-if="form.format">
+  Write the content as {{ form.format }}.
+  </template>
+  <template v-if="form.outputLength">
+  ### Length
+  The content length should be {{ form.outputLength }}.
+  </template>
 </template>
 <template v-if="form.style.length">
 ### Language style and tone
@@ -79,8 +56,35 @@ Make sure to cover these actions or steps in your answer:
 ### Additional notes
 Please take into consideration the following additional notes:
 {{ form.notes }}
-</template>
-</code></pre>
+</template>`.replaceAll("\n<template", "<template").replace('\n  ', '\n');
+
+const Result = {
+  props: ["form"],
+  methods: {
+    async copyToClipboard() {
+      const resultText = document.getElementById("result-code").innerText;
+      const button = document.getElementById("copy-button");
+
+      try {
+        await navigator.clipboard.writeText(resultText);
+        console.log("Copied to clipboard!");
+
+        button.innerText = "Copied!";
+
+        setTimeout(() => {
+          button.innerText = "Copy";
+        }, 2000);
+      } catch (err) {
+        console.error("Failed to copy: ", err);
+      }
+    },
+  },
+  template: `
+    <div>
+      <h2>Result</h2>
+      <p>Paste this prompt into your AI assistant:</p>
+      <button id="copy-button" class="button" role="button" @click="copyToClipboard">Copy</button>
+      <pre><code id="result-code">${outputCode}</code></pre>
     </div>
   `,
 };
