b6365120d3619ed0beafb024aa6d5a246a0c2ad2
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2025-09-10 11:54:26 +0200

    update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ const Intro = {
       It offers a user-friendly form which guides you to enter input and the process is even easier by choosing from preset values.
       Finally, all the inputs are combined as single prompt with structure and wording that LLM tools will understand. Whether for creative writing, business, or coding.
     </p>
-    <p>Depending on your use case, you can tell the LLM to ask questions before responding, or to just make assumptions and to proceed without asking for further info.
+
     </p>
     <p><b>Recommended</b>: See one of the tools listed here under Prompt Writing Tools to optimize your prompt or check the Prompt Libraries to get ideas from existing prompts. Go to <a href="https://michaelcurrin.github.io/dev-resources/resources/artificial-intelligence/">AI - Dev Resources</a>.</p>
   `,
@@ -28,6 +28,12 @@ const Instructions = {
         <p>Fill in the form and see the prompt generated immediately. When you are done, click "Copy".</p>
         <p>Your answers will be persisted so the form remembers what you entered. But everything is kept private and stored locally - your inputs are not sent across the internet.</p>
         <p>Only the "Request" field is required; all other fields are optional. Any empty fields will be excluded from the output to maintain brevity.</p>
+        <p><b>Tip:</b> If you leave out a field that makes sense (e.g. audience for a blog post), the LLM may ask for info before proceeding. This can help quality but may slow you down. What could you do? Tell the LLM what you want for consistent results</p>
+        <ul>
+          <li>Ask for thoroughness: "Ask me questions before responding until you have all the necessary info".</li>
+          <li>Optimize for speed: "Choose for me, make assumptions, and proceed without asking for further info. Tell me what you chose before writing the piece".</li>
+          <li>Let it choose for a specific field, e.g. "Audience: Choose for me based on what is relevant for this request.</li>
+        </ul>
       </details>
     </div>
   `,
